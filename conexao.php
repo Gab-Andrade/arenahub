14bc6ae180df4d33b94fc1b822e0e9fd5c98ce54
Gabriel Andrade <gabriel.andradelima0407@gmail.com>
2026-03-12 10:26:22 -0300

    Criando o arquivo de conexão com o banco de dados

---
+++ conexao.php
@@ -0,0 +1,18 @@
+<?php
+
+$host = 'localhost';
+$dbname = 'arenahub';
+$usuario = 'root';
+$senha = '';       
+
+try {
+    $pdo = new PDO("mysql:host=$host;dbname=$dbname;charset=utf8", $usuario, $senha);
+    
+    $pdo->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    
+    echo "Conexão com o banco do ArenaHub realizada com sucesso!"; 
+    
+} catch (PDOException $e) {
+    die("Erro ao conectar com o banco de dados: " . $e->getMessage());
+}
+?>
